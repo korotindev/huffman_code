0685f30dcaf6972188d120f0bf55650622758063
Dmitry Korotin <korotin.dev@gmail.com>
2021-01-27 12:08:34 +0000

    implement cached bits reading

--- src/cached_bit_reader.cpp
+++ src/cached_bit_reader.cpp
@@ -8,8 +8,29 @@
 const size_t CachedBitReader::MAX_CACHED_BYTES = 10;
 
 CachedBitReader::CachedBitReader(std::istream& in) : in_(in) { buffer.resize(MAX_CACHED_BYTES); }
-bool CachedBitReader::HasBits() const { return false; }
-bool CachedBitReader::ReadBit() { return false; }
+
+bool CachedBitReader::HasBits() const { 
+  return in_ || bucket_idx < buckets_cached;
+}
+
+bool CachedBitReader::ReadBit() { 
+  if (bucket_bit_idx == 8) {
+    bucket_idx++;
+    bucket_bit_idx = 0;
+  }
+
+  if (bucket_idx == buckets_cached) {
+    CacheNextChunk();
+    bucket_idx = 0;
+    bucket_bit_idx = 0;
+  } 
+
+  bool bit = buffer[bucket_idx] & 1;
+  buffer[bucket_idx] = buffer[bucket_idx] >> 1;
+  bucket_bit_idx++;
+
+  return bit;
+}
 
 void CachedBitReader::CacheNextChunk() {
   ReadPrimitive(in_, buffer.data(), MAX_CACHED_BYTES);
